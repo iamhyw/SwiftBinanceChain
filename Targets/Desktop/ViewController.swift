9e5335fcef8240a9809cc5b26f21127e9973e5b2
Michael Henderson <roadkillrabbit@gmail.com>
2019-04-13 19:08:50 +1000

    Desktop TextView

--- Targets/Desktop/ViewController.swift
+++ Targets/Desktop/ViewController.swift
@@ -1,8 +1,10 @@
 import Cocoa
 import BinanceChain
 
-class ViewController: NSViewController {
+class ViewController: NSViewController, TestDelegate {
 
+    @IBOutlet weak var textField: NSTextField!
+    
     private var test: Test?
     
     override func viewDidLoad() {
@@ -10,9 +12,25 @@ class ViewController: NSViewController {
 
         // Run tests
         self.test = Test()
+        self.test?.delegate = self
         self.test?.runAllTestsOnTestnet()
 
     }
 
+    // MARK: - TestDelegate
+
+    func testComplete(label: String, property: Any, error: Error?) {
+
+        var text = label + ":\n"
+        if let error = error {
+            text += error.localizedDescription
+        } else {
+            text += String(describing: property)
+        }
+
+        self.textField.stringValue = text + "\n\n" + self.textField.stringValue
+
+    }
+    
 }
 
