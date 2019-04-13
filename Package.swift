f2effe57b3ebcd0d34df899fe84a68c3b94c0d70
Michael Henderson <roadkillrabbit@gmail.com>
2019-04-13 19:08:26 +1000

    Added HDWallet dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,12 @@ import PackageDescription
 
 let package = Package(
     name: "BinanceChain",
+    platforms: [
+        .macOS(.v10_11),
+        .iOS(.v11),
+        .tvOS(.v11),
+        .watchOS(.v2)
+    ],
     products: [
         .library(name: "BinanceChain", targets: ["BinanceChainCore"]),
     ],
@@ -10,17 +16,18 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-protobuf.git", from: "1.4.0"),
         .package(url: "https://github.com/Alamofire/Alamofire.git", from: "4.8.2"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.0"),
-        .package(url: "https://github.com/0xDEADP00L/Bech32.git", from: "1.0.0"),
         .package(url: "https://github.com/SwiftyJSON/SwiftyJSON.git", from: "4.3.0"),
         .package(url: "https://github.com/DaveWoodCom/XCGLogger.git", from: "7.0.0"),
-        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.0"),
         .package(url: "https://github.com/malcommac/SwiftDate", from: "6.0.1"),
-        .package(url: "git@github.com:kylef/Commander.git", from: "0.8.0")
+        .package(url: "https://github.com/kylef/Commander.git", from: "0.8.0"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.0.0"),
+        .package(url: "https://github.com/Boilertalk/secp256k1.swift.git", from: "0.1.4"),
+        .package(url: "https://github.com/mh7821/HDWallet.git", .revision("f246f8beaeee84356125e60f5550cf9a74191166"))
     ],
     targets: [
         .target(name: "BinanceChainCore",
-                dependencies: ["SwiftProtobuf", "Alamofire", "SwiftyJSON", "Starscream",
-                               "Bech32", "XCGLogger", "secp256k1", "SwiftDate", "Commander"],
+                dependencies: ["SwiftProtobuf", "Alamofire", "SwiftyJSON", "Starscream", "HDWalletKit",
+                               "XCGLogger", "secp256k1", "SwiftDate", "Commander", "CryptoSwift"],
                 path: "BinanceChain/Sources")
     ]
 )
