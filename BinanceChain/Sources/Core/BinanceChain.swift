6bd3e8571d67b19bdc7b1363610b77ba48038a6f
Michael Henderson <roadkillrabbit@gmail.com>
2019-04-15 16:21:49 +1000

    Moved HexEncoding into Util

--- BinanceChain/Sources/Core/BinanceChain.swift
+++ BinanceChain/Sources/Core/BinanceChain.swift
@@ -285,23 +285,5 @@ public class BinanceChain {
         return request
 
     }
-
-    // MARK: - Alamofire Util
-
-    private struct HexEncoding: ParameterEncoding {
-
-        private let data: Data
-
-        init(data: Data) {
-            self.data = data
-        }
-
-        func encode(_ urlRequest: URLRequestConvertible, with parameters: Parameters?) throws -> URLRequest {
-            var urlRequest = try urlRequest.asURLRequest()
-            urlRequest.setValue("text/plain", forHTTPHeaderField: "Content-Type")
-            urlRequest.httpBody = data.hexdata
-            return urlRequest
-        }
-    }
     
 }
